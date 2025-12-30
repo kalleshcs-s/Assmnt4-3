c5c8b06606d047de02b65baa42022714708e612c
kalleshcs <kalleshcs@gmail.com>
2025-12-30 20:53:01 +0530

    fourth update

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 module "vpc" {
-  source       = "./modules/vpc"
+  source       = "./modules/Assmnt4-3/vpc"
   env          = var.env
   vpc_cidr     = var.vpc_cidr
   subnet_cidrs = var.subnet_cidrs
 }
 
 module "ec2" {
-  source             = "./modules/ec2"
+  source             = "./modules/Assmnt4-3/ec2"
   env                = var.env
   ami_id             = var.ami_id
   subnet_id          = module.vpc.subnet_ids[0]
