78615128cc8203b94a51d55ff8c9f1810db0d9bd
kalleshcs <kalleshcs@gmail.com>
2026-01-07 20:21:01 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,18 @@
+module "vpc" {
+  source   = "./vpc"
+  name     = "dev"
+  vpc_cidr = "10.0.0.0/16"
+  subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  azs      = ["us-east-1a", "us-east-1b"]
+  env      = "dev"
+}
+
+module "ec2" {
+  source          = "./ec2"
+  name            = "dev"
+  ami             = "ami-08dcd9e62169fad6e"
+  instance_type   = "t2.micro"
+  subnet_id       = module.vpc.subnet_ids[0]
+  security_groups = []
+  message         = "Hello from DEV EC2"
+}
